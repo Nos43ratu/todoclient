f04a8c46e261c019be393f19cc6d3818ed64f565
nos43ratu <kairolla_adil2@mail.ru>
2020-05-05 14:39:45 +0300

    Updates

---
+++ precache-manifest.56684fbd0dd9d786c176d03cebdf324e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f902a5484f6f0c7653ae05e266a3fc62",
+    "url": "/todoclient/index.html"
+  },
+  {
+    "revision": "f665a11e9444dadc8c43",
+    "url": "/todoclient/static/css/2.2bd37e2f.chunk.css"
+  },
+  {
+    "revision": "f665a11e9444dadc8c43",
+    "url": "/todoclient/static/js/2.aef6d212.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/todoclient/static/js/2.aef6d212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05030ba55b27632b3236",
+    "url": "/todoclient/static/js/main.f64efa8b.chunk.js"
+  },
+  {
+    "revision": "0f57a75c7cccc805d738",
+    "url": "/todoclient/static/js/runtime-main.bb8458a7.js"
+  }
+]);
