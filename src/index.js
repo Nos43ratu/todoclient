78c2a04f5c7a6c3badc68ab0a9109acd6eea7af1
nos43ratu <kairolla_adil2@mail.ru>
2020-05-07 12:54:17 +0300

    heroku app crashed fix2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/storeConfig/store";
-import Todo from "./todo";
+import Todo from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
